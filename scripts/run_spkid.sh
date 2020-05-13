fcbe4fe7e25db27fb4648f7e1a6f9e60657fa9dd
David PAV ETSETB <david.pav@upc.edu>
2020-05-13 22:34:20 +0200

    keep pushing

--- scripts/run_spkid.sh
+++ scripts/run_spkid.sh
@@ -18,10 +18,10 @@ name_exp=one
 db=spk_8mu/speecon
 
 nfilt_mfcc=20
-mfcc_order=12
+mfcc_order=13
 order_lpc=8
-order_lpcc=10
-order_lpcc_cep=15
+order_lpcc=16
+order_lpcc_cep=20
 thr=0.001
 nmixtures=8
 gmm_ini=1 
@@ -112,7 +112,7 @@ compute_lpcc() {
 compute_mfcc() {
     for filename in $(cat $lists/class/all.train $lists/class/all.test); do
         mkdir -p `dirname $w/$FEAT/$filename.$FEAT`
-        EXEC="wav2mfcc $mfcc_order $nfilt_mfcc 8 $db/$filename.wav $w/$FEAT/$filename.$FEAT"
+        EXEC="wav2mfcc $mfcc_order $nfilt_mfcc  $db/$filename.wav $w/$FEAT/$filename.$FEAT"
         echo $EXEC && $EXEC || exit 1
     done
 }
@@ -147,7 +147,8 @@ for cmd in $*; do
        for dir in $db/BLOCK*/SES* ; do
            name=${dir/*\/}
            echo $name ----
-           gmm_train  -v 1 -T 0.001 -N5 -m 4 -d $w/$FEAT -e $FEAT -g $w/gmm/$FEAT/$name.gmm $lists/class/$name.train || exit 1
+          # gmm_train  -v 1 -T 0.001 -N5 -m 20 -d $w/$FEAT -e $FEAT -g $w/gmm/$FEAT/$name.gmm $lists/class/$name.train || exit 1
+           gmm_train  -v 1 -T 0.00001 -n 40 -m 40 -d $w/$FEAT -e $FEAT -g $w/gmm/$FEAT/$name.gmm $lists/class/$name.train || exit 1
            echo
        done
    elif [[ $cmd == test ]]; then
@@ -171,8 +172,7 @@ for cmd in $*; do
 	   #
 	   # - The name of the world model will be used by gmm_verify in the 'verify' command below.
         #echo $name ----
-        #  gmm_train  -v 1 -T 0.001 -N5 -m 4 -d $w/$FEAT -e $FEAT -g $w/gmm/$FEAT/$name.gmm $lists/class/$name.train || exit 1
-      # gmm_train  -v 1 -T $thr -N5 -m  $nmixtures -d $w/$FEAT -e $FEAT -g $w/gmm/$FEAT/world.gmm $lists/verif/users_and_others.train || exit 1
+       gmm_train  -v 1 -T 0.001 -N5 -m 8 -d $w/$FEAT -e $FEAT -g $w/gmm/$FEAT/world.gmm $lists/verif/users_and_others.train || exit 1
         #echo
        echo "Implement the trainworld option ..."
    elif [[ $cmd == verify ]]; then
@@ -184,7 +184,8 @@ for cmd in $*; do
 	   #   For instance:
 	   #   * <code> gmm_verify ... > $w/verif_${FEAT}_${name_exp}.log </code>
 	   #   * <code> gmm_verify ... | tee $w/verif_${FEAT}_${name_exp}.log </code>
-       echo "Implement the verify option ..."
+       gmm_verify -d $w/$FEAT -e $FEAT -D $w/gmm/$FEAT -E gmm -w world $lists/gmm.list $lists/verif/all.test $lists/verif/all.test.candidates > $w/verif_${FEAT}_${name_exp}.log || exit 1
+      # echo "Implement the verify option ..."
 
    elif [[ $cmd == verif_err ]]; then
        if [[ ! -s $w/verif_${FEAT}_${name_exp}.log ]] ; then
